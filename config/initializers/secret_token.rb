2785c869967e4b7e9f4ddf555d64dd4925d8dbec
brian1987 <pisano.b@gmail.com>
2014-03-11 18:44:55 -0400

    initial commit, welcome to seatgeek y'all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TicktrProject::Application.config.secret_key_base = '381ea29143004e9c7a35d459a3e4ca811af4ab2f340d9ee197ef504fca0ea738ca5de45c7e4377f28ba6543d167519b44c16bf881e6d9523bb7f2a43bcc49aa5'
