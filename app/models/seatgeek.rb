c00eaaed7ea317460870976b875c3bdbad7be725
brian1987 <pisano.b@gmail.com>
2014-03-11 19:28:40 -0400

    newest stuff

--- app/models/seatgeek.rb
+++ app/models/seatgeek.rb
@@ -9,3 +9,9 @@ end
 SeatGeek::Connection.events({:type => "concert"})
 SeatGeek::Connection.events({:type => "baseball"})
 SeatGeek::Connection.events({:type => "sports"})
+SeatGeek::Connection.events({:type => "theater"})
+SeatGeek::Connection.events({:type => "hockey"})
+SeatGeek::Connection.events({:type => "basketball"})
+SeatGeek::Connection.events({:type => "soccer"})
+
+
