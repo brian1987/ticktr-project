bd0d39e5822f5d8a3ef63ac9a433235788e2e8ed
brian1987 <pisano.b@gmail.com>
2014-03-12 17:54:43 -0400

    minor change

--- app/models/seat_geek_api_connect.rb
+++ app/models/seat_geek_api_connect.rb
@@ -1,4 +1,3 @@
-# require 'seatgeek'
 class SeatGeekApiConnect
 
   attr_accessor :events
@@ -7,18 +6,7 @@ class SeatGeekApiConnect
     @postal_code = postal_code
   end
 
-  # def get_sports
-  #   #binding.pry
-  #   formatted_sports_events = []
-  #   @events = SeatGeek::Connection.events({:type => "sports", :postal_code => @postal_code})
-  #   @events["events"].each do |event|
-  #     formatted_sports_events << {:url => event["url"], :title => event["title"]}
-  #   end
-  #   formatted_sports_events
-  # end
-
   def get_events
-    #binding.pry
     formatted_events = []
     until formatted_events.empty? == false
       @event_type = %w(sports concerts baseball theater soccer basketball band nba mlb mls nhl boxing monster_truck).sample
@@ -29,59 +17,5 @@ class SeatGeekApiConnect
     end
     formatted_events
   end
-
-
-  # def get_baseball
-  #   #binding.pry
-  #   formatted_baseball_events = []
-  #   @events = SeatGeek::Connection.events({:type => "baseball", :postal_code => @postal_code})
-  #   @events["events"].each do |event|
-  #     formatted_baseball_events << {:url => event["url"], :title => event["title"]}
-  #   end
-  #   formatted_baseball_events
-  # end
-
-  # def get_theater
-  #   #binding.pry
-  #   formatted_theater_events = []
-  #   @events = SeatGeek::Connection.events({:type => "theater", :postal_code => @postal_code})
-  #   @events["events"].each do |event|
-  #     formatted_theater_events << {:url => event["url"], :title => event["title"]}
-  #   end
-  #   formatted_theater_events
-  # end
-
-  #  def get_soccer
-  #   #binding.pry
-  #   formatted_soccer_events = []
-  #   @events = SeatGeek::Connection.events({:type => "soccer", :postal_code => @postal_code})
-  #   @events["events"].each do |event|
-  #     formatted_soccer_events << {:url => event["url"], :title => event["title"]}
-  #   end
-  #   formatted_soccer_events
-  # end
-
-  # def get_basketball
-  #   #binding.pry
-  #   formatted_basketball_events = []
-  #   @events = SeatGeek::Connection.events({:type => "basketball", :postal_code => @postal_code})
-  #   @events["events"].each do |event|
-  #     formatted_basketball_events << {:url => event["url"], :title => event["title"]}
-  #   end
-  #   formatted_basketball_events
-  # end
-
-
 end
-# s = SeatGeekApiConnect.new(11215)
-# puts s.get_theater
-
-
-
-# SeatGeek::Connection.events({:type => "theater"})
-# SeatGeek::Connection.events({:type => "hockey"})
-# SeatGeek::Connection.events({:type => "basketball"})
-# SeatGeek::Connection.events({:type => "soccer"})
-# h["events"].first["url"]
-# h["events"].first["short_title"]
-
+  
