02c25c49cf772e5885724dd3d5e96a421b87cde8
Joan Soskin <joan.e.soskin@gmail.com>
2014-03-12 21:08:55 -0400

    added zip code validation to user class

--- app/models/seat_geek_api_connect.rb
+++ app/models/seat_geek_api_connect.rb
@@ -1,9 +1,6 @@
 class SeatGeekApiConnect
 
   attr_accessor :events
-  validates_format_of :postal_code,
-   :with => /^\d{5}(-\d{4})?$/, 
-   :message => "should be in the form 12345 or 12345-1234"
 
   def initialize(postal_code)
     @postal_code = postal_code
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 require 'pry'
 class User < ActiveRecord::Base
 
+  validates_format_of :postal_code,
+   :with => /\A\d{5}-\d{4}|\A\d{5}\z/, 
+   :message => "...? WTF"
 
 	# def like(event_type)
 	# 	if event_type == "concert"
