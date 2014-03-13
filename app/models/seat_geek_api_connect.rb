c26be215dde9aef00f921e0a4f3b564737998020
Joan Soskin <joan.e.soskin@gmail.com>
2014-03-12 20:51:16 -0400

    testing postal code validation

--- app/models/seat_geek_api_connect.rb
+++ app/models/seat_geek_api_connect.rb
@@ -1,6 +1,9 @@
 class SeatGeekApiConnect
 
   attr_accessor :events
+  validates_format_of :postal_code,
+   :with => /^\d{5}(-\d{4})?$/, 
+   :message => "should be in the form 12345 or 12345-1234"
 
   def initialize(postal_code)
     @postal_code = postal_code
