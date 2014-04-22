89c2cc3bb5aa2519c1452d136ecd31d16b72bd86
brian1987 <pisano.b@gmail.com>
2014-04-22 13:47:36 -0400

    deployed?

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,21 @@
+load 'deploy/assets'
 require 'bundler/capistrano'
 
 set :application, "ticktr-project"
 set :repository,  "git@github.com:brian1987/ticktr-project.git"
 
-set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+#set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-default_run_options[:pty] = true
 
 set :user, 'brian'
 set :deploy_to, "/home/#{ user }/#{ application }"
 set :use_sudo, false
-# set :default_run_options[:pty] = true
+
+default_run_options[:pty] = true
 
 role :web, "162.243.66.69"                          # Your HTTP server, Apache/etc
 role :app, "162.243.66.69"                          # This may be the same as your `Web` server
+
 # role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
